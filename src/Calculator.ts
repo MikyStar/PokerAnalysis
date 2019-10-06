003eaeef9fddec3529a1a8a21079ba546dc780d1
Mike AUBENAS <mike.aubenas@gmail.com>
2019-10-07 00:14:46 +0200

    WIP on calculator structure

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -4,10 +4,77 @@ export class Calculator
 {
 	hands : any[];
 	line : Card[];
+	timestamp : TimeStamp;
+	target : number;
 
-	constructor( hands : any[], line : Card[] )
+	constructor( hands : any[], line : Card[], timestamp : TimeStamp , target : number )
 	{
 		this.hands = hands;
 		this.line = line;
+		this.timestamp = timestamp;
+		this.target = target;
 	}
+
+	printAllStats()
+	{
+		
+	}
+
+	highCard()
+	{
+
+	}
+
+	simplePair()
+	{
+
+	}
+
+	twoPair()
+	{
+
+	}
+
+	threeOfAKind()
+	{
+
+	}
+
+	straight()
+	{
+
+	}
+
+	flush()
+	{
+
+	}
+
+	fullHouse()
+	{
+
+	}
+
+	fourOfAKind()
+	{
+
+	}
+
+	straightFlush()
+	{
+
+	}
+
+	royalFlush()
+	{
+
+	}
+}
+
+export enum TimeStamp
+{
+	NONE = 'none',
+	FLOP = 'flop',
+	TURN = 'turn',
+	REVERSE = 'reverse'
 }
--- src/_main.ts
+++ src/_main.ts
@@ -1,5 +1,6 @@
 import { Game } from './Game';
 import { Player } from './Player';
+import { TimeStamp, Calculator } from './Calculator'
 
 ////////////////////////////////////////////////////////////
 
@@ -11,11 +12,14 @@ const START_LITTLE_BLIND = 5;
 let players = createPlayers( NUMBER_OF_PLAYERS );
 
 let game = new Game( players, START_LITTLE_BLIND );
+let timestamps = [ TimeStamp.NONE, TimeStamp.FLOP, TimeStamp.TURN, TimeStamp.REVERSE ]
+let line = game.makeTheLine();
 
 ////////////////////////////////////////////////////////////
 
 printPlayersHands();
 printLine();
+printStats();
 
 ////////////////////////////////////////////////////////////
 
@@ -44,9 +48,38 @@ function printLine()
 {
 	console.log( '\n-----LINE-----' );
 
-	let line = game.makeTheLine();
-
 	console.log( 'FLOP : ', line[ 0 ], line[ 1 ], line [ 2 ] );
 	console.log( 'TURN : ', line[ 3 ] );
 	console.log( 'REVERSE : ', line[ 4 ] );
+}
+
+function printStats()
+{
+	console.log( '\n-----STATS-----\n' );
+
+	players.forEach( ( player, id ) =>
+	{
+		console.log( `Player ${ id }\n` );
+
+		timestamps.forEach( timestamp =>
+		{
+			let calculator = new Calculator( getHands(), line, timestamp, id );
+
+			calculator.printAllStats();
+		})
+	})
+
+	////////////////////////////////////////////////
+
+	function getHands() : any[]
+	{
+		let hands : any[] = [];
+
+		players.forEach( player =>
+		{
+			hands.push( [ player.hand[ 0 ], player.hand[ 1 ] ] );
+		});
+
+		return hands;
+	}
 }
