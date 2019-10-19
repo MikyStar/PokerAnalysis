8be9fffccb4dbe6a8f30f4b7927f8513e48f4d9c
AUBENAS Mike <mike.aubenas@gmail.com>
2019-10-19 19:46:43 +0200

    Can print cards + icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Card } from './components/Card';
+import { Suit, Face } from './model/Card';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+////////////////////////////////////////////////////////////////////
+
+const App : React.FC = () =>
+{
+	return 	(
+				<div>
+
+					<Card
+						id={ Face.KING }
+						suit={ Suit.CUBS }
+					/>
+
+					<Card
+						id={ 10 }
+						suit={ Suit.HEART }
+					/>
+				</div>
+			);
 }
 
 export default App;
---
+++ src/components/UserHole.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Suit, Face } from '../model/Card'
+import './Card.css'
+
+///////////////////////////////////////////////////
+
+type UserHoleProps =
+{
+}
+
+///////////////////////////////////////////////////
+
+export const Card = ( { } : UserHoleProps ) =>
+(
+	<>
+	</>
+)
